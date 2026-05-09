13d9e152e1081528970f6939e62c7b0f066157b9
Ashif Ahmed Shuvo <shuvoasifahmed@gmail.com>
2026-05-10 04:02:06 +0600

    Update icons and dependencies in project configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.aas.smartherbo.BAU',
   appName: 'SmartHerbo',
-  webDir: 'dist'
+  webDir: 'dist',
+  icon: '/assets/icon.png'  
 };
 
 export default config;
